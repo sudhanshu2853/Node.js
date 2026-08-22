9a21f16fd843807c7145437514eaf55af1b0c1cd
Sudhanshu <sudhanshuk2v@gmail.com>
2026-08-22 21:44:05 +0530

    external modules update e.g. nodemon,colors

--- colors.js
+++ colors.js
@@ -1,4 +1,5 @@
  const color= require('colors');
 
  console.log(color.yellow('Hii This is Sudhanshu'));
+ console.log(color.green.bgWhite('Hii This is Sudhanshu'));
  
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-
-    res.write('<h1 align="center">Welcome</h1>');
+   res.write('<h1 align="center">Welcome to Server version 1.0</h1>');
+    
     res.end("Hello User! \n How are You?")
-}).listen(4800);
+}).listen(3000);
+
+
+http.createServer((req, res) => {
+    res.write('<h1 align="center">Welcome to Server version 2.0</h1>');
+    res.end();
+}).listen(3200);
